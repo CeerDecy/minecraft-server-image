6bebd8ed5e3b5a4ce92d61373a15f08c3e9106f6
CeerDecy <1748788674@qq.com>
2024-02-29 11:09:24 +0800

    feat: customize the jre environment or automatically parse and install jre based on version

---
+++ minecraft-server-dynamic-version/image-tools/parse-jre.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+tag="$1"
+version="${tag#*.}"
+version="${version%.*}"
+# 根据不同的条件输出不同的值
+if (( version >= 1 && version <= 6 )); then
+  echo "openjdk-8-jre"
+elif (( version >= 7 && version <= 12 )); then
+  echo "openjdk-8-jre"
+elif (( version >= 13 && version <= 16 )); then
+  echo "openjdk-11-jre"
+elif (( version >=17 && version <= 18 )); then
+  echo "openjdk-17-jre"
+else
+  echo "openjdk-17-jre"
+fi
